d17494699a40e64c9fd061ea57ea8b8d2d441696
Lejandary21 <159848652+Lejandary21@users.noreply.github.com>
2024-09-19 11:32:30 +1200

    Add files via upload

---
+++ menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://node_2d.tscn")
+
+
+
+func _on_option_pressed():
+	pass # Replace with function body.
+
+
+
+func _on_quit_pressed():
+	pass # Replace with function body.
